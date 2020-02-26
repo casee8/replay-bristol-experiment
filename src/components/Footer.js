8486654dc724cb3a2bf154f2b72adccc743ff5b0
casee8 <pokerkaszy7@gmail.com>
2020-02-26 18:53:37 +0100

    Add content to 'Footer' & 'About' components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import replayLogo from '../media/img/transparent_icon.png';
 
 const Footer = () => {
-  return <div>Hello from Footer</div>;
+  return (
+    <div className='footer-parent'>
+      <div className='footer-grid'>
+        <div className='footer-img-links'>
+          <img src={replayLogo} alt='' />
+          <div className='footer-links'>
+            <p>Follow us on:</p>
+            <div>
+              <a
+                href='https://facebook.com/ReplayBristol'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fab fa-facebook-f'></i> Facebook
+              </a>
+              <a
+                href='https://twitter.com/Replay_Bristol'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fab fa-twitter'></i> Twitter
+              </a>
+              <a
+                href='https://instagram.com/replaybristol'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fab fa-instagram'></i> Instagram
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className='footer-cr'>
+          <p>Copyright &copy; 2020</p>
+        </div>
+        <div className='footer-pp'>
+          <a href='#' target='_blank'>
+            Privacy Policy
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,44 +1,48 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import playing_with_friends from '../../media/img/playing_with_friends.jpeg';
 import img_3878 from '../../media/img/IMG_3878.jpeg';
+import img_party from '../../media/img/45418583_10215360889673413_5576257152516882432_n.jpeg';
+import img_bparty from '../../media/img/birtday_party.jpg';
+import img_3736 from '../../media/img/IMG_3736.jpeg';
+import img_4084 from '../../media/img/IMG_4084.jpeg';
 
 class About extends Component {
   render() {
     return (
-      <div>
-        <div className='background'></div>
+      <div className='main-div'>
+        {/* <div className='background'></div> */}
         <div className='white-bckgrnd'>
           <div className='container'>
             <h1 className='secondary-font text-center extra-padding'>
               What is Replay?
             </h1>
-            <div className='fifty-fifty'>
-              <div>
-                <div>
-                  <p>
-                    Replay is a bar where you can enjoy playing with your
-                    friends while having a drink or nibbling snacks whether it
-                    is a platter of nachos, a bowl of nuts or wasabi peas.
-                  </p>
-                </div>
-                <div>
-                  <p>
-                    We also offer delicious coffees, locally sourced loose leaf
-                    teas and alcoholic beverages. But more importantly, Replay
-                    is about the passion for board games and play activities.
-                  </p>
-                </div>
-                <div>
-                  <p>
-                    Our aim is to share this passion with you so you can
-                    experience the importance of joint engagement, and hopefully
-                    pass it on to others.
-                  </p>
-                </div>
+            <div className='grid-content'>
+              <div className='first-grid card'>
+                <p>
+                  Replay is a bar where you can enjoy playing with your friends{' '}
+                  <i className='fas fa-user-friends'></i> while having a drink
+                  or nibbling snacks whether it is a platter of nachos, a bowl
+                  of nuts or wasabi peas.
+                </p>
+                <img className='first-img' src={playing_with_friends} alt='' />
               </div>
-              <div>
-                <img className='two-imgs' src={playing_with_friends} alt='' />
-                <img className='two-imgs sec-img' src={img_3878} alt='' />
+              <div className='second-grid card'>
+                <img className='sec-img' src={img_3878} alt='' />
+                <p>
+                  We also offer delicious coffees{' '}
+                  <i className='fas fa-coffee'></i>, locally sourced loose leaf
+                  teas and alcoholic beverages. But more importantly, Replay is
+                  about the passion for <i className='fas fa-dice'></i> board
+                  games and play activities.
+                </p>
+              </div>
+              <div className='third-grid card'>
+                <p className='text-center'>
+                  Our <i className='fas fa-crosshairs'></i> aim is to share this
+                  passion with you so you can experience the importance of joint
+                  engagement, and hopefully pass it on to others.
+                </p>
               </div>
             </div>
             <hr />
@@ -49,57 +53,88 @@ class About extends Component {
         </div>
         <div className='container'>
           <p className='transparent-bckgrnd'>
-            There is a £3 play charge per for a 3 hrs stay, for this you can
-            play with as many games as you want from our 300 board games
-            library. You can play ping pong in our basement which is free
-            between Tuesday and Thursday, and £3 for half an hour between Friday
-            and Sunday. Please book a table so we expect you coming.
+            <strong>
+              There is a £3 play charge per <i className='fas fa-user'></i> for
+              a 3 hrs stay
+            </strong>
+            , for this you can play with as many games as you want from our 300
+            board games library. You can play ping pong in our basement which is
+            free between Tuesday and Thursday, and £3 for half an hour between
+            Friday and Sunday.{' '}
+            <Link to='/contact'>
+              Please book a table so we expect you coming.
+            </Link>{' '}
+            <i className='far fa-smile-wink'></i>
           </p>
         </div>
         <div className='white-bckgrnd'>
-          <div className='container'>
-            <h1 className='secondary-font text-center'>Why visiting us?</h1>
-            <div>
-              <img alt='' />
+          <div className='container grid'>
+            <h1 className='secondary-font text-center grid-one'>
+              Why visiting us?
+            </h1>
+            <div className='grid-two card'>
+              <div>
+                <img src={img_3736} alt='' />
+                <p>
+                  <i className='far fa-hand-point-right'></i> Because we have a{' '}
+                  <strong>more than 300 board game</strong>{' '}
+                  <i className='fas fa-stream'></i> collection including the
+                  newest releases and most popular ones.
+                </p>
+              </div>
+            </div>
+            <div className='grid-three card'>
+              <img src={img_4084} alt='' />
               <p>
-                Because we have a more than 300 board game collection including
-                the newest releases and most popular ones.
+                <strong>
+                  <i className='far fa-hand-point-right'></i> We are here to{' '}
+                  <i className='far fa-life-ring'></i> help you
+                </strong>{' '}
+                choose the most suitable games for you depending on the number
+                of players, the level of experience and the required time
+                duration.
               </p>
             </div>
-            <div>
-              <img alt='' />
+            <div className='grid-four card'>
               <p>
-                We are here to help you choose the most suitable games for you
-                depending on the number of players, the level of experience and
-                the required time duration.
+                <strong>
+                  <i className='far fa-hand-point-right'></i> In case you are
+                  planning to buy a new board game{' '}
+                  <i className='fas fa-chess'></i>
+                </strong>
+                , you can try it here before you buy it! If you decide to buy
+                the game, we are able to help you with 5% discount at Rules of
+                Play, St. Nicholas Market.
               </p>
             </div>
-            <div>
+            <div className='grid-five card'>
               <p>
-                In case you are planning to buy a new board game, you can try it
-                here before you buy it! If you decide to buy the game, we are
-                able to help you with 5% discount at Rules of Play, St. Nicholas
-                Market.
+                <strong>
+                  <i className='far fa-hand-point-right'></i> You can enjoy{' '}
+                  <i className='fas fa-utensils'></i> food and{' '}
+                  <i className='fas fa-cocktail'></i> drinks
+                </strong>{' '}
+                while trying to beat your opponents.
               </p>
             </div>
-            <div>
+            <div className='grid-six card'>
               <p>
-                You can enjoy food and drinks while trying to beat your
-                opponents.
-              </p>
-            </div>
-            <div>
-              <img alt='' />
-              <img alt='' />
-              <p>
-                We host special events, organize Birthday parties, team
+                <i className='far fa-hand-point-right'></i>{' '}
+                <strong>We host special events</strong>, organize{' '}
+                <i className='fas fa-birthday-cake'></i> Birthday parties, team
                 buildings, hen and stag dos. Get in touch for prices and
                 availability!
               </p>
+              <div className='grid-imgs'>
+                <img src={img_party} alt='' />
+                <img src={img_bparty} alt='' />
+              </div>
             </div>
-            <div>
+            <div className='grid-seven card'>
               <p>
-                We do tournaments! Are you good at ping pong or table football?
+                <i className='far fa-hand-point-right'></i>{' '}
+                <strong>We do tournaments!</strong> Are you good at ping pong{' '}
+                <i className='fas fa-table-tennis'></i> or table football?
                 Challenge yourself at our in house tournament! To get to know
                 dates please subscribe to our website or follow us on social
                 media.
